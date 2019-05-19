a6addaf55c09950ef55c23bdf62193b18ccf5b06
theseriousadult <jack@gallabytes.com>
2019-05-18 17:24:14 -0700

    swap default behavior, bump version

--- src/main.rs
+++ src/main.rs
@@ -57,19 +57,19 @@ fn main() {
         .version("0.1")
         .arg(Arg::with_name("DIR").help("directory to checksum").index(1))
         .arg(
-            Arg::with_name("sum")
+            Arg::with_name("serial")
                 .short("-s")
-                .long("--sum")
-                .help("checksum directory instead of serially hashing"),
+                .long("--serial")
+                .help("serially hash directory instead of checksumming"),
         )
         .get_matches();
 
     let path = args.value_of("INPUT").unwrap_or_else(|| ".");
     let walker = WalkDir::new(Path::new(path));
-    let val = if args.is_present("sum") {
-        sum_dir(walker).unwrap_or_else(|e| panic!("{:?}", e))
-    } else {
+    let val = if args.is_present("serial") {
         hash_dir(walker).expect("couldn't checksum - found no files")
+    } else {
+        sum_dir(walker).unwrap_or_else(|e| panic!("{:?}", e))
     };
     println!("{:08x}", val);
 }
